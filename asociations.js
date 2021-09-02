d39d7c7f42659cbb745e20717d67cbe3f73d37d5
Julian Ajras <julianajras@Julians-Mac-mini.local>
2021-09-02 01:31:25 -0300

    mysql remote

--- asociations.js
+++ asociations.js
@@ -1,27 +1,54 @@
-// const sequelize = require('sequelize')
-// const Character = require('./models/Character')
-// const Genre = require('./models/Genre')
-// const Movie = require('./models/Movie')
+const sequelize = require('sequelize')
+const {Character} = require('./database/db')
+const {Genre} = require('./database/db')
+const {Movie} = require('./database/db')
 
 
-// module.exports = (sequelize, type) => {
-//     return sequelize.define('charactermovie', {
-//     MovieId: {
-//       type: type.INTEGER,
-//       references: {
-//         model: Movie, // 'Movies' would also work
-//         key: 'id'
-//       }
-//     },
-//     CharacterId: {
-//       type: type.INTEGER,
-//       references: {
-//         model: Character, // 'Actors' would also work
-//         key: 'id'
-//       }
-//     }
-    
-//   });
-//   Movie.belongsToMany(Character, { through: charactermovie })
-//   Character.belongsToMany(Movie, { through: charactermovie })
-// }
+module.exports = (sequelize, type) => {
+    return sequelize.define('charactermovie', {
+    MovieId: {
+      type: type.INTEGER,
+      references: {
+        model: Movie,
+        key: 'id'
+      }
+      
+    },
+    CharacterId: {
+      type: type.INTEGER,
+      references: {
+        model: Character,
+        key: 'id'
+      }
+    },
+  
+})
+
+}
+
+module.exports = (sequelize, type) => {
+    return sequelize.define('genremovie', {
+    MovieId: {
+      type: type.INTEGER,
+      references: {
+        model: Movie, 
+        key: 'id'
+      }
+      
+    },
+    CharacterId: {
+      type: type.INTEGER,
+      references: {
+        model: Genre, 
+        key: 'id'
+      }
+    },
+  
+})
+
+}
+
+Movie.belongsToMany(Character, { through: 'charactermovie' })
+Character.belongsToMany(Movie, { through: 'charactermovie' })
+Movie.belongsToMany(Genre, { through: 'genremovie' })
+Genre.belongsToMany(Movie, { through: 'genremovie' })
--- database/db.js
+++ database/db.js
@@ -1,40 +1,27 @@
-// const Sequelize = require('sequelize')
+const Sequelize = require('sequelize')
 
-// const MovieModel = require('./models/Movie')
-// const UserModel = require('./models/User')
-// const CharacterModel = require('./models/Character')
-// const GenreModel = require('./models/Genre')
+const MovieModel = require('../models/Movie')
+const UserModel = require('../models/User')
+const CharacterModel = require('../models/Character')
+const GenreModel = require('../models/Genre')
 
 
-// const sequelize = new Sequelize('X8fOhDOaim','X8fOhDOaim','BueRyyWGr7',{host:'remotemysql.com', dialect: 'mysql'})
+const sequelize = new Sequelize('X8fOhDOaim','X8fOhDOaim','BueRyyWGr7',{host:'remotemysql.com', dialect: 'mysql'})
 
-// const Movie = MovieModel(sequelize, Sequelize)
-// const User = UserModel(sequelize, Sequelize)
-// const Character = CharacterModel(sequelize, Sequelize)
-// const Genre = GenreModel(sequelize, Sequelize)
+const Movie = MovieModel(sequelize, Sequelize)
+const User = UserModel(sequelize, Sequelize)
+const Character = CharacterModel(sequelize, Sequelize)
+const Genre = GenreModel(sequelize, Sequelize)
 
-// sequelize.sync({force:false})
-//     .then(() =>{
-//         console.log('tablas sincronizadas')
-//     })
+sequelize.sync({force:false})
+    .then(() =>{
+        console.log('tablas sincronizadas')
+    })
 
-//     module.exports = {
-//         Movie,
-//         User,
-//         Genre,
-//         Character
-//     }
-
-const {Sequelize} = require('sequelize')
-const {database} = require('./config')
-
-const sequelize = new Sequelize(
-    database.database,
-    database.username,
-    database.password, {
-        host: database.host,
-        dialect: "mysql"
+    module.exports = {
+        Movie,
+        User,
+        Genre,
+        Character
     }
-);
 
-module.exports = sequelize;
